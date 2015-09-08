fdb397cb15b86b25bb89657bb1758fdb5e9fc2ca
Sigurgrímur Unnar Ólafsson <sigurgrimur13@ru.is>
2015-09-08 21:03:39 +0000

    working on it...

--- src/tftpd.c
+++ src/tftpd.c
@@ -8,5 +8,17 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
-        
+    int port_n;
+    char **file_path;
+    //Check if the number of arguments is correct
+    if(argc != 3){
+        //Later change this to present a menu asking for destination n' stuff
+        printf("Number of arguments incorrect.\n");
+    }
+
+    port_n = argv[1];
+    file_path = argv[2];
+
+
+    return 0;
 }
