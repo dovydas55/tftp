fb8bcda0145b5a48bb353cd734f8aef8cd791ea3
Sigurgrímur Unnar Ólafsson <sigurgrimur13@ru.is>
2015-09-08 20:43:03 +0000

    added a tftpd c file to work on

---
+++ src/tftpd.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+        
+}
